a53eac1b0ca3e783982cf18af5ed6fc43b4e9b79
IF-TI <italofurquim@gmail.com>
2022-09-02 14:39:42 -0300

    Concluido!

---
+++ Estudos/Entendendo Web-Pack/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const meuNome = 'Ítalo';
+
+ReactDOM.render(    
+    <div>Nome: {meuNome}</div>,
+    document.querySelector('#app')
+)
+
+console.log('Funcionou porra!')
